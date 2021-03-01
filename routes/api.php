de167973c079205def518cdc2c7fbd8fda822a8b
IvanWebSiteDev <lordofagile@gmail.com>
2021-03-01 19:06:16 +0200

    rev 1.0

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/game/simulate/season', '\App\Http\Controllers\ApiJson\SimulatorController@simulateSeason');
+Route::post('/game/simulate/playoff', '\App\Http\Controllers\ApiJson\SimulatorController@simulatePlayoff');
